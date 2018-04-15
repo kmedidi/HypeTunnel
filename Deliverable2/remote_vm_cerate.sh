4c7fcb3ab7000b48251f990407ba50173f151a35
Manoj Sharan Gunasegaran <gms.94@icloud.com>
2018-04-14 20:01:03 -0400

    Update remote_vm_cerate.sh

--- Deliverable2/remote_vm_cerate.sh
+++ Deliverable2/remote_vm_cerate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# sudo bash <full_path_to_remote_vm_create.sh>/remote_vm_create.sh <TenantID> <VMNAME> <VM_NEWIP> <VXLAN_TUNNELID> <VM_HYPERVISOR_IP> <Other-hypervisor_IP_1> <Other_Hypervisor_IP_2> ...
 SELF_HYP_IP=$5
 
 # PREPARE THE EXECUTABLE FILE
