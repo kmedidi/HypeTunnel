2e3b482de0ffa9ac8ecc55554779907e92822a85
kmedidi <kmedidi@ncsu.edu>
2018-04-26 13:53:39 -0400

    add_vm.sh

--- FinalDeliverable/add_vm.sh
+++ FinalDeliverable/add_vm.sh
@@ -6,7 +6,7 @@
 IFS=. read -r i1 i2 i3 i4 <<< $(echo $1 | cut -d/ -f1)
 PREFIX=$(echo $1 | cut -d/ -f2)
 IFS=. read -r xx m1 m2 m3 m4 <<< $(for a in $(seq 1 32); do if [ $(((a - 1) % 8)) -eq 0 ]; then echo -n .; fi; if [ $a -le $PREFIX ]; then echo -n 1; else echo -n 0; fi; done)
-GW=$(printf "%d.%d.%d.%d\n" "$((i1 & (2#$m1)))" "$((i2 & (2#$m2)))" "$((i3 & (2#$m3)))" "$((i4 & (2#$m4)))")
+GW=$(printf "%d.%d.%d.%d\n" "$((i1 & (2#$m1)))" "$((i2 & (2#$m2)))" "$((i3 & (2#$m3)))" "$((i4 & (2#$m4))+1)")
 #-----------------------------------------------------------------------
 
 #Create container if it doesn't exist
