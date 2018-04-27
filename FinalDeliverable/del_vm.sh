95b05e8f656cf3c75a852b037a24035bad816c9b
Charan Ram <35636004+cvellai@users.noreply.github.com>
2018-04-27 13:56:59 -0400

    Update del_vm.sh

--- FinalDeliverable/del_vm.sh
+++ FinalDeliverable/del_vm.sh
@@ -1,3 +1,36 @@
 #!/bin/sh
 
 ############################## SHELL SCRIPT TO DELETE TENANT CONTAINERS/VMS ###################################
+
+TID = $1
+C_NAME=$2
+#C_IP = $3
+#MAC = $4
+
+#State: present/absent
+State=$(sudo docker ps -a | grep -c "<\$C_NAME\>")
+if [[ $State == 1 ]]
+then
+  #echo "$C_NAME is present on Hypervisor"
+  #Status =$(sudo docker ps -a --format "table {{.Names}}\t{{.Status}}"| grep "\<$VM_NAME\>" | gawk '{{ print $2 }}')
+  Status=$(sudo docker inspect -f {{.State.Status}} $C_NAME)
+  if [[ $Status == "running" ]]
+  then
+    #echo "Shutting down $VM_NAME...."
+    sudo docker stop $C_NAME
+  else if [[ $Status == "paused" ]]
+  then
+    sudo docker unpause $C_NAME
+    sudo docker stop $C_NAME
+  fi
+  #echo "Removing $C_NAME...."
+  sudo docker rm $C_NAME
+  State=$(sudo docker ps -a | grep -c "<\$C_NAME\>")
+  if [[ $State == 0 ]];then
+    #echo "Successfully removed $C_NAME"
+    echo "true"
+  fi
+else
+  #echo " Container not present! Enter the correct Container name."
+  echo "false"
+fi
