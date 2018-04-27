cfe3e70f89c7c09bd577741a961ae58bc6b16ee9
Kashyap <ksivasu@ncsu.edu>
2018-04-27 13:14:30 -0400

    Update tenant_infra.sh

--- FinalDeliverable/tenant_infra.sh
+++ FinalDeliverable/tenant_infra.sh
@@ -1,3 +1,22 @@
-#!/bin/sh
 
 ########################### SHELL SCRIPT TO CREATE INITIAL TENANT INFRASTRUCTURE PER HYPERVISOR ################################
+#!/bin/bash
+id=$1
+create_flag=$2
+if [ "$create_flag" == "true" ]
+then
+netns_name=T"$id"_NS
+sudo ip netns add $netns_name
+sudo ip link add T"$id"NS_hyp type veth peer name hyp_T"$id"NS
+sudo ip link set T"$id"NS_hyp up
+sudo ip link set hyp_T"$id"NS up
+sudo ip addr add 10.0."$id".2/24 dev hyp_T"$id"NS
+sudo ip link set T"$id"NS_hyp netns T"$id"_NS
+sudo ip netns exec T"$id"_NS ip link set T"$id"NS_hyp up
+sudo ip netns exec T"$id"_NS ip addr add 10.0."$id".1/24 dev T"$id"NS_hyp
+sudo ip netns exec T"$id"_NS sudo iptables -t nat -A POSTROUTING -o T"$id"NS_hyp -j MASQUERADE
+sudo ip netns exec T"$id"_NS ip route add default via 10.0."$id".2
+sudo iptables -t nat -A POSTROUTING -s 10.0."$id".0/24 -j MASQUERADE
+else
+sudo ip netns del T"$id"_NS
+fi
