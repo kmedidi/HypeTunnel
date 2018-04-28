8a45f5eeee31ea24b36e6625aeee12da5de70211
Kashyap <ksivasu@ncsu.edu>
2018-04-27 22:14:55 -0400

    Update add_vm.sh

--- FinalDeliverable/add_vm.sh
+++ FinalDeliverable/add_vm.sh
@@ -6,7 +6,8 @@
 IFS=. read -r i1 i2 i3 i4 <<< $(echo $2 | cut -d/ -f1)
 PREFIX=$(echo $2 | cut -d/ -f2)
 IFS=. read -r xx m1 m2 m3 m4 <<< $(for a in $(seq 1 32); do if [ $(((a - 1) % 8)) -eq 0 ]; then echo -n .; fi; if [ $a -le $PREFIX ]; then echo -n 1; else echo -n 0; fi; done)
-GW=$(printf "%d.%d.%d.%d\n" "$((i1 & (2#$m1)))" "$((i2 & (2#$m2)))" "$((i3 & (2#$m3)))" "$((i4 & (2#$m4))+1)")
+GW=$(printf "%d.%d.%d.%d\n" "$((i1 & (2#$m1)))" "$((i2 & (2#$m2)))" "$((i3 & (2#$m3)))" "$((((i4 & (2#$m4)))+1))")
+echo "GATEWAY" $GW
 #-----------------------------------------------------------------------
 
 #Create container if it doesn't exist
@@ -15,19 +16,24 @@ vm_dup=$(sudo docker ps -a | grep -c $1)
 if [[ vm_dup -eq 0 ]]
 then
   sudo docker run -itd --name $1 ubuntu
-  sudo docker exec $1 apt-get update
-  sudo docker exec $1 apt-get install iputils-ping
-  sudo docker exec $1 apt-get install iproute2
+  sudo docker exec $1 apt-get update > /dev/null
+  sudo docker exec $1 apt-get install -y iputils-ping > /dev/null
+  sudo docker exec $1 apt-get install -y iproute2 > /dev/null
   #Create veth pair and attach it to the central_ovs and container
   sudo ip link add $1_0 type veth peer name $1_1
   sudo ip link set $1_0 up
   sudo ovs-vsctl add-port central_ovs $1_0 tag=$3
-  pid="$(sudo docker inspect -f '{{.State.Pid}}' "$1")"
-  sudo ip link set netns $(pid) dev $1_1
-  sudo nsenter -t $(pid) -n ip link set $1_1 up
-  sudo nsenter -t $(pid) -n ip addr add $2 dev $1_1
-  sudo nsenter -t $(pid) -n ip route del default
-  sudo nsenter -t $(pid) -n ip route add default via $GW dev $1_1
-  sudo docker inspect --format='{{.NetworkSettings.MacAddress}}' $(docker ps | grep -o '[a-z0-9]\{12\}')
-#Add a flow entry in central_ovs with the mac using $3 - tenantid
+  pid="$(sudo docker inspect -f {{.State.Pid}} "$1")"
+  sudo ip link set netns $pid dev $1_1
+  sudo nsenter -t $pid -n ip link set $1_1 up
+  sudo nsenter -t $pid -n ip addr add $2 dev $1_1
+  sudo nsenter -t $pid -n ip route del default
+  sudo nsenter -t $pid -n ip route add default via $GW
 fi
+IP_ADD=$(sudo docker exec $1 ip addr)
+
+#sudo docker inspect --format='{{.NetworkSettings.MacAddress}}' $1
+#sudo docker attach $1
+mac=$(sudo docker exec $1 cat /sys/class/net/$1_1/address)
+echo $mac
+#sudo docker inspect --format='{{.NetworkSettings.MacAddress}}' $(docker ps | grep -o '[a-z0-9]\{12\}')
