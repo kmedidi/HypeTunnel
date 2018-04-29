b47febd10e0ad9b1169f4b2155f6cb14a8335bdb
Charan Ram <35636004+cvellai@users.noreply.github.com>
2018-04-29 17:56:33 -0400

    Update del_vm.sh

--- FinalDeliverable/del_vm.sh
+++ FinalDeliverable/del_vm.sh
@@ -1,26 +1,31 @@
 
 #!/bin/bash
 ##################  SHELL SCRIPT TO DELETE TENANT CONTAINERS/VMs ON THE SPECIFIED HYPERVISOR  ########################
+
 TID=$1
 C_NAME=$2
 MOVE=$3
 
+echo "INSIDE DEL_VM"
 #State: present/absent
-State=$(sudo docker ps -a | grep -c "<\$C_NAME\>")
+State=$(sudo docker ps -a | grep -c "\<$C_NAME\>")
 if [[ $State == 1 ]]
 then
-  #echo "$C_NAME is present on Hypervisor"
+  echo "del_vm: $C_NAME is present on Hypervisor"
   #Status =$(sudo docker ps -a --format "table {{.Names}}\t{{.Status}}"| grep "\<$VM_NAME\>" | gawk '{{ print $2 }}')
   if [[ $MOVE == "true" ]]
   then
-    sudo docker commit $C_NAME ${C_NAME}_image
-    sudo docker save ${C_NAME}_image >  $HOME/${C_NAME}_image.tar
-    file="$HOME/${C_NAME}_image.tar"
-    if [ -f $file ];then
-      echo "True"
+    echo " del_vm: Inside del_vm MOVE block!!!"
+    cont=$(echo "$C_NAME" | tr '[:upper:]' '[:lower:]')
+    sudo docker commit $C_NAME ${cont}_image
+    sudo docker save ${cont}_image >  $HOME/${cont}_image.tar
+    file="$HOME/${cont}_image.tar"
+    if [ -f $file ]
+    then
+    echo "del_vm: True MOVED"
     else
       ###Image tar not created###
-      echo "False"
+      echo "del_vm: False MOVED"
     fi
   else
     Status=$(sudo docker inspect -f {{.State.Status}} $C_NAME)
@@ -28,20 +33,28 @@ then
     then
       #echo "Shutting down $VM_NAME...."
       sudo docker stop $C_NAME
-    else if [[ $Status == "paused" ]]
+    elif [[ $Status == "paused" ]]
     then
       sudo docker unpause $C_NAME
       sudo docker stop $C_NAME
     fi
     #removing any moved container images
-    file="$HOME/${C_NAME}_image.tar"
-    if [ -f $file ];then
+    cont=$(echo "$C_NAME" | tr '[:upper:]' '[:lower:]')
+    IMG=$(sudo docker images | grep -c "\<${cont}_image\>")
+    if [[ $IMG == 1 ]]
+    then
+      sudo docker rmi ${cont}_image > /dev/null
+    fi
+    file="$HOME/${cont}_image.tar"
+    if [ -f $file ]
+    then
       rm $file
     fi
     #echo "Removing $C_NAME...."
-    sudo docker rm $C_NAME
-    State=$(sudo docker ps -a | grep -c "\<$C_NAME\>")
-    if [[ $State == 0 ]];then
+    sudo docker rm $C_NAME > /dev/null
+    State=$(sudo docker ps -a | grep -c "<\$C_NAME\>")
+    if [[ $State == 0 ]]
+    then
       #echo "Successfully removed $C_NAME"
       echo "True"
     else
@@ -53,3 +66,8 @@ else
     #echo " Container not present! Enter the correct Container name."
     echo "False"
 fi
+
+
+
+
+
