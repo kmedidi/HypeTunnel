9bcea1fe2ee2aed6130f68374182010b7c032a7f
Charan Ram <35636004+cvellai@users.noreply.github.com>
2018-04-29 01:28:51 -0400

    Update del_vm.sh

--- FinalDeliverable/del_vm.sh
+++ FinalDeliverable/del_vm.sh
@@ -1,11 +1,9 @@
-#!/bin/sh
 
-############################## SHELL SCRIPT TO DELETE TENANT CONTAINERS/VMS ###################################
+#!/bin/bash
 
 TID = $1
 C_NAME=$2
-#C_IP = $3
-#MAC = $4
+MOVE=$3
 
 #State: present/absent
 State=$(sudo docker ps -a | grep -c "<\$C_NAME\>")
@@ -13,24 +11,40 @@ if [[ $State == 1 ]]
 then
   #echo "$C_NAME is present on Hypervisor"
   #Status =$(sudo docker ps -a --format "table {{.Names}}\t{{.Status}}"| grep "\<$VM_NAME\>" | gawk '{{ print $2 }}')
-  Status=$(sudo docker inspect -f {{.State.Status}} $C_NAME)
-  if [[ $Status == "running" ]]
+  if [[ $MOVE == "True" ]]
   then
-    #echo "Shutting down $VM_NAME...."
-    sudo docker stop $C_NAME > /dev/null
-  else if [[ $Status == "paused" ]]
-  then
-    sudo docker unpause $C_NAME > /dev/null
-    sudo docker stop $C_NAME > /dev/null
-  fi
-  #echo "Removing $C_NAME...."
-  sudo docker rm $C_NAME > /dev/null
-  State=$(sudo docker ps -a | grep -c "<\$C_NAME\>")
-  if [[ $State == 0 ]];then
-    #echo "Successfully removed $C_NAME"
-    echo "True"
+    sudo docker commit $C_NAME ${C_NAME}_Image
+    sudo docker save ${C_NAME}_Image >  $HOME/${C_NAME}_Image.tar
+    file="$HOME/${C_NAME}_Image.tar"
+    if [ -f $file ];then
+      echo "true"
+    else
+      ###Image tar not created###
+      echo "false"
+    fi
+  else
+    Status=$(sudo docker inspect -f {{.State.Status}} $C_NAME)
+    if [[ $Status == "running" ]]
+    then
+      #echo "Shutting down $VM_NAME...."
+      sudo docker stop $C_NAME
+    else if [[ $Status == "paused" ]]
+    then
+      sudo docker unpause $C_NAME
+      sudo docker stop $C_NAME
+    fi
+    #echo "Removing $C_NAME...."
+    sudo docker rm $C_NAME
+    State=$(sudo docker ps -a | grep -c "<\$C_NAME\>")
+    if [[ $State == 0 ]];then
+      #echo "Successfully removed $C_NAME"
+      echo "true"
+    else
+      ###Container not removed###
+      echo "false"
+    fi
   fi
 else
-  #echo " Container not present! Enter the correct Container name."
-  echo "False"
+    #echo " Container not present! Enter the correct Container name."
+    echo "false"
 fi
