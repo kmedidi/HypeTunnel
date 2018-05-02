9ffced6dfbc6fb8409a6501c965a7d5106b83899
Kashyap <ksivasu@ncsu.edu>
2018-05-02 00:57:40 -0400

    Update man_flows.sh

--- FinalDeliverable/man_flows.sh
+++ FinalDeliverable/man_flows.sh
@@ -5,7 +5,11 @@ out_int=$(sudo ovs-vsctl show | grep -B 2 $2 | grep Interface\ \"vxlan | cut -d 
 out_port=$(sudo ovs-ofctl show tunnel_ovs | grep $out_int | cut -d '(' -f 1 | cut -d ' ' -f 2)
 sudo ovs-ofctl add-flow tunnel_ovs "table=1,tun_id=$3,dl_dst=$4,actions=output:$out_port"
 sudo ovs-ofctl add-flow tunnel_ovs "table=1,tun_id=$3,arp,nw_dst=$(echo $5 | cut -d '/' -f 1),actions=output:$out_port"
+ip_ad=$(echo $5 | cut -d '/' -f 1)
+sudo ip netns exec T$3_NS arp -s $ip_ad $4
 else
 sudo ovs-ofctl del-flows tunnel_ovs "tun_id=$3,dl_dst=$4"
 sudo ovs-ofctl del-flows tunnel_ovs "tun_id=$3,nw_dst=$5"
+ip_ad=$(echo $5 | cut -d '/' -f 1)
+sudo ip netns exec T$3_NS arp -d $ip_ad
 fi
